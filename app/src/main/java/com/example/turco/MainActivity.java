f6d2e61fbe2de5a47aec593cc60fbf0d4f357ee6
Daniel Osterkamp <danielosterkamp38@gmail.com>
2026-09-16 19:31:51 -0300

    tela vencedor

--- app/src/main/java/com/example/turco/MainActivity.java
+++ app/src/main/java/com/example/turco/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.turco;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +17,15 @@ public class MainActivity extends AppCompatActivity {
     int pontosUm;
     int pontosDois;
 
+    TextView textoUm;
+    TextView textoDois;
+
+    TextView editPontos;
+
+    TextView editEquipeUm;
+    TextView editEquipeDois;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,24 +37,116 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        textoUm = findViewById(R.id.pontosEquipeUm);
+        textoDois = findViewById(R.id.pontosEquipeDois);
+
+        editPontos = findViewById(R.id.editPontos);
+        editEquipeDois = findViewById(R.id.editEquipe2);
+        editEquipeUm = findViewById(R.id.editUm);
+
+
         pontosUm = 0;
         pontosDois = 0;
     }
 
-    public void adicionarNumeroUm(){
+    public void adicionarNumeroUm(View view){
+
+        if (verificacao()){
+            int proximoNumero = pontosUm + 1;
+
+
+                pontosUm = proximoNumero;
+
+
+            textoUm.setText(String.valueOf(proximoNumero));
+
+            encontraVencedor();
+
+        }
 
     }
 
-    public void adicionarNumeroDois(){
+    public void adicionarNumeroDois(View view){
+
+        if (verificacao()){
+            int proximoNumero = pontosDois + 1;
+
+
+                pontosDois = proximoNumero;
+
+            textoDois.setText(String.valueOf(proximoNumero));
+
+            encontraVencedor();
+        }
+    }
+
+    public void diminuirNumeroUm(View view){
+
+        if (verificacao()){
+            int proximoNumero = pontosUm - 1;
+
+            if (proximoNumero > 0){
+                pontosUm = proximoNumero;
+                textoUm.setText(String.valueOf(proximoNumero));
+            }
+
+
+        }
 
     }
 
-    public void diminuirNumeroUm(){
+    public void diminuirNumeroDois(View view){
+
+        if (verificacao()){
+            int proximoNumero = pontosDois - 1;
+
+            if (proximoNumero >= 0){
+                pontosDois = proximoNumero;
+                textoDois.setText(String.valueOf(proximoNumero));
+            }
+
+
+        }
+
 
     }
 
-    public void diminuirNumeroDois(){
+    public boolean verificacao(){
+        if (editEquipeUm.getText().toString().isEmpty() || editEquipeDois.getText().toString().isEmpty()){
+            Toast.makeText(getApplicationContext(), "Adicione os nomes primeiro", Toast.LENGTH_SHORT).show();
+            return false;
+        }else if (editPontos.getText().toString().isEmpty()){
+            Toast.makeText(getApplicationContext(), "Sem pontuação máxima", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        try {
+            int num = Integer.parseInt(editPontos.getText().toString());
+        }catch (NumberFormatException e){
+            Toast.makeText(getApplicationContext(), "Pontuação máxima invalida", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return true;
+    }
+
+    public void encontraVencedor (){
+
+        int pontosMaximo = Integer.parseInt(editPontos.getText().toString());
+
+        Intent intent = new Intent(this, MainActivity2.class);
+
+        if (pontosUm>=pontosMaximo){
+            intent.putExtra("Vencedor", editEquipeUm.getText().toString());
+            startActivity(intent);
+
+        }else if (pontosDois >= pontosMaximo){
+            intent.putExtra("Vencedor", editEquipeDois.getText().toString());
+            startActivity(intent);
+
+        }
 
     }
+
 
 }
--- app/src/main/java/com/example/turco/MainActivity2.java
+++ app/src/main/java/com/example/turco/MainActivity2.java
@@ -3,6 +3,7 @@ package com.example.turco;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,11 @@ public class MainActivity2 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        textView = findViewById(R.id.textView5);
+        String nome = getIntent().getStringExtra("Vencedor");
+        String texto = nome + " Venceu!";
+        textView.setText(texto);
     }
 
     public void toInicial(View view){
